65c4e283d350fda1c9728ed68c85482725298d03
Cod-E-Tron701 <jerin.cherian005@gmail.com>
2024-04-14 16:05:02 +0530

    #1

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -17,7 +17,7 @@
 		loading = true;
 		const { data, error } = await supabase.from('leaderboard').select('name, points');
 		if (error) console.log('Error Fetching Teams:', error.message);
-		if (data.length) leaderboard = data.sort((a, b) => a.points - b.points); // Sort by points
+		if (data.length) leaderboard = data.sort((a, b) => b.points - a.points); // Sort by points
 		loading = false;
 	}
 
